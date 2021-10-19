95d18432ceb823437cb5187eba6c9bec748d2241
Pquar <pquar@hotmail.com>
2021-10-18 23:37:18 -0300

    styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import Head from 'next/head';
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
-
+import useStyles from '../utils/styles';
 export default function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>This page has a title 🤔</title>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>Gear Doge</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
         <Typography>Todos direitos reservados. Guide Doge.</Typography>
       </footer>
     </div>
