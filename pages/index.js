45d8b7440f1fe551e69b9f39c7796a76ac85be0e
Pquar <pquar@hotmail.com>
2021-10-26 17:34:15 -0300

    main

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,45 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import Header from '../components/Header.js';
+import data from '../utils/data';
 import Layout from '../components/Layout.js';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  CardActions,
+  Typography,
+  Button,
+} from '@material-ui/core';
 
 export default function Home() {
   return (
     <Layout>
       <div>
-        <h1>Produtos </h1>
-        <ul>
-          <li>produto 1</li>
-          <li>produto 2</li>
-          <li>produto 2</li>
-        </ul>
+        <h1>Produtos</h1>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>${product.price}</Typography>
+                  <Button size="small" color="primary">
+                    Adicione ao Carrinho
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   );
